eecfe2986d9d400a0004b3966697df838790985a
Milo Arnerich Hatch <miloshcavitch@gmail.com>
2016-05-02 16:37:34 -0400

    adds animation to project description. bug remains

--- DOM.js
+++ DOM.js
@@ -1,10 +1,12 @@
-var projectsRay = [{name: "HUE WARS", description: "blah", link: "#"},
-                {name: "FUNFETTI", description: "blah", link: "#"},
-                {name: "PIXEL SMOKE", description: "blah", link: "#"},];
-
+var projectsRay = [{name: "HUE WARS", description: "Elitr nemore prodesset sea ei, cum et quando aeterno. Congue causae salutatus duo ne, nam id hinc eripuit omittantur, an rationibus honestatis eloquentiam eos.", link: "#"},
+                {name: "FUNFETTI", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", link: "#"},
+                {name: "PIXEL SMOKE", description: "Quidam mediocrem eam ei. Vim partiendo liberavisse ad. Ut omnis consulatu est, duo nibh zril iisque, no.", link: "#"},];
+var titleIndex = 0;
 var titleSwitch = true;
 $('#ta').css('left', '-150%');
 $('#ta').css('display', 'none');
+$('#da').css('left', '150%');
+$('#da').css('display', 'none');
 var updateProject = function(){
   var inToT, outOfT, inToD, outOfD;
   if (titleSwitch === true){
@@ -19,13 +21,26 @@ var updateProject = function(){
     outOfD = '#da';
   }
   $(inToT).css('display', 'block');
+  $(inToD).css('display', 'block');
+  $(inToT).empty();
+  $(inToD).empty();
+  $(inToT).append("<a href='" + projectsRay[titleIndex].link + "' <h2>" + projectsRay[titleIndex].name + "</h2></a>");
+  $(inToD).append("<p>" + projectsRay[titleIndex].description + "</p>");
   $(outOfT).css('left', '150%');
+  $(outOfD).css('left', '-150%');
   setTimeout(function(){
     $(outOfT).css('display', 'none');
+    $(outOfD).css('display', 'none');
     $(outOfT).css('left', '-150%');
+    $(outOfD).css('left', '225%');
   }, 750);
   setTimeout(function(){
     $(inToT).css('left', '25%');
+    $(inToD).css('left', '50%');
   }, 10)
   titleSwitch = !titleSwitch;
+  titleIndex++;
+  if (titleIndex === projectsRay.length -1){
+    titleIndex = 0;
+  }
 }
